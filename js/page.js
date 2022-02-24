aa4f504faa1dd654419137408da2c72eb8a13584
Delaney Cifranik-Stovall <delaney.cifranik-stovall@student.awesomeinc.org>
2022-02-24 21:23:17 +0000

    properties in JSON to indicate what is present for each page

--- js/page.js
+++ js/page.js
@@ -2,31 +2,62 @@ const pages = {
     "page1": [ //has instructions and go button
         {
             //properties
+            "instructions": "I can read your mind.",
+            "nextButton": null,
+            "underButtonMessage": null,
+            "goOrRefresh": "GO",
+            "scrollSection": null
         }
     ],
     "page2": [
         {
             //properties
+            "instructions": "Pick a number from 01 - 99",
+            "nextButton": "Next",
+            "underButtonMessage": "when you have your number, click next",
+            "goOrRefresh": "back",
+            "scrollSection": null
         }
     ],
     "page3": [
         {
             //properties
+            "instructions": "Add both digits together to get a new number",
+            "nextButton": "Next",
+            "underButtonMessage": "Ex: 14 is 1 + 4  = 5 \n click next to proceed",
+            "goOrRefresh": "back",
+            "scrollSection": null
         }
     ],
     "page4": [
         {
             //properties
+            "instructions": "Subtract your new number from your old number",
+            "nextButton": "Next",
+            "underButtonMessage": "Ex: 14 - 5 = 9 \n click next to proceed",
+            "goOrRefresh": "back",
+            "scrollSection": null
         }
     ],
     "page5": [
         {
             //properties
+            "instructions": "",
+            "nextButton": "Reveal",
+            "underButtonMessage": "Find your new number. \n Note the symbol beside the number",
+            "goOrRefresh": "back",
+            "scrollSection": "numbers and symbols"
+            
         }
     ],
     "page6": [
         {
             //properties
+            "instructions": "symbol produced",
+            "nextButton": null,
+            "underButtonMessage": "Your symbol is: \n ",
+            "goOrRefresh": "reset",
+            "scrollSection": null
         }
     ]
 }

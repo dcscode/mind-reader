01d3827d81ca842436333b4d0c3a4f9ba382e830
Delaney Cifranik-Stovall <delaney.cifranik-stovall@student.awesomeinc.org>
2022-02-24 20:19:04 +0000

    add array of pages, add skeleton event listeners, add skeleton functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //get all the elements
 
-
+const pages = ["page1", "page2", "page3", "page4", "page5", "page6"]; //index 0 - 5
 //current page
 //certain elements are hidden depending on which page (1-6)
 const gamePage = document.getElementById("game-page");
@@ -8,9 +8,9 @@ const gamePage = document.getElementById("game-page");
 //instructions, larger text on top; present on every page
 const instructions = document.getElementById("instructions");
 
-
 //div for scroll section on page 5
 const scrollSection = document.getElementById("scroll-section");
+
 //div for next/reveal button and under button message
 //unhidden on pages 2-5
 const buttonGroup = document.getElementById("button-group");
@@ -19,6 +19,7 @@ const buttonGroup = document.getElementById("button-group");
 //next on pages 2-4
 //reveal on page 5
 //hidden on page 6
+//changes page, moves forward
 const nextButton = document.getElementById("next-button");
 
 //smaller message under next button
@@ -26,20 +27,60 @@ const nextButton = document.getElementById("next-button");
 const underButtonMessage = document.getElementById("under-button");
 
 //go on start page
-//go becomes go back on pages 2-6
+//go becomes go back on pages 2-6; returns to the previous page
 //on page 6, refresh resets the game
 const goOrRefreshButton = document.getElementById("go-or-refresh");
 
 
 
 
-goOrRefreshButton.addEventListener("click", //refresh or go function
+goOrRefreshButton.addEventListener("click", //startGame function
 );
+
+goOrRefreshButton.addEventListener("click", //goBack function
+)
+
+goOrRefreshButton.addEventListener("click", //reset function
+)
+
+goOrRefreshButton.addEventListener("click", //randomize function; after reset/part of reset
+)
+
+nextButton.addEventListener("click", //move forward; next function
+)
+
+nextButton.addEventListener("click", //updatePageContent function
+)
+
+
 
 
 
 
 function startGame() { //start game when you click go
+    //maybe bundle into INIT
 
 }
+
+function next() { //next page/reveal; move forward
+
+}
+
+function goBack() { //return to the previous page
+
+}
+
+function reset() { //resets the game at the end, when go back on page 6 is clicked
+
+}
+
+function updatePage() { //update page content
+
+} 
+
+function randomize() { //randomize symbols every game, after reset is clicked
+
+}
+
+
 
